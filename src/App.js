860098a00a78add57e5585a0b7721976b3c5608f
Mattthew Lusty <mattlusty123@gmail.com>
2022-01-16 11:44:01 +0000

    feat: add Login page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import CV from "./components/CV";
+import Login from "./components/Login";
 
 function App() {
   let [hidden, setHidden] = useState(false);
@@ -17,15 +18,20 @@ function App() {
 
   return (
     <div className="App">
-      <Nav toggleSideMenu={toggleSideMenu} />
-      <SideMenu toggleSideMenu={toggleSideMenu} hidden={hidden} />
       <Switch>
-        <Route path="/portfolio">
-          <Portfolio> </Portfolio>
+        <Route path="/login" component={Login} />
+        <Route path="/">
+          <Nav toggleSideMenu={toggleSideMenu} />
+          <SideMenu toggleSideMenu={toggleSideMenu} hidden={hidden} />
+          <Switch>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/contact" component={Contact} />
+            <Route path="/cv" component={CV} />
+            <Route path="/" component={About} />
+          </Switch>
         </Route>
-        <Route path="/contact" component={Contact} />
-        <Route path="/cv" component={CV} />
-        <Route path="/" component={About} />
       </Switch>
     </div>
   );
---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+import "../styles/css/Login.css";
+
+function Login() {
+  return (
+    <div className="Login">
+      <div className="login">
+        <div className="header">
+          <div className="matt">Matthew</div>
+          <div className="lusty">Lusty</div>
+        </div>
+        <form className="loginForm">
+          <div className="username">
+            <label htmlFor="username">Username</label>
+            <input id="username"></input>
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input id="password"></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
