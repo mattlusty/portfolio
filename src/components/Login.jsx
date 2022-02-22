8d2db567f99cb479b626cdc4e5d8d7f52f1e4bbe
Mattthew Lusty <mattlusty123@gmail.com>
2022-02-22 14:15:00 +0000

    feat: add Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import { withRouter, Link } from "react-router-dom";
+import * as http from "../services/httpService";
 
 import "../styles/css/Login.css";
 
 function Login(props) {
-  var login = () => {
-    props.history.push("about");
+  var [creds, setCreds] = useState({ username: "", password: "" });
+
+  var login = async (e) => {
+    e.preventDefault();
+    const { username, password } = creds;
+    const endPoint = "http://localhost:4000/portfolio/login";
+    http
+      .post(endPoint, { username, password })
+      .then((res) => {
+        window.localStorage.setItem("accessToken", res.data.token);
+        console.log(res.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  var handleChange = (e) => {
+    var _creds = { ...creds };
+    _creds[e.currentTarget.name] = e.currentTarget.value;
+    setCreds(_creds);
   };
 
   return (
@@ -18,16 +39,16 @@ function Login(props) {
         <div className="register">
           Need an account? <Link to="/register">Register</Link>
         </div>
-        <form>
+        <form onSubmit={login}>
           <div className="field username">
             <label htmlFor="username">Username</label>
-            <input id="username"></input>
+            <input onChange={handleChange} id="username" name="username" value={creds.username}></input>
           </div>
           <div className="field password">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password"></input>
+            <input onChange={handleChange} type="password" id="password" name="password" value={creds.password}></input>
           </div>
-          <button onClick={login}>Login</button>
+          <button>Login</button>
         </form>
       </div>
     </div>
