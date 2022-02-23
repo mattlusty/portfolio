00a0c6a62e92db96121f0dcd1f6db829ef4689c8
Mattthew Lusty <mattlusty123@gmail.com>
2022-02-23 11:36:05 +0000

    feat: add error state for Register validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,6 +6,20 @@ import "../styles/css/Register.css";
 
 function Register(props) {
   var [account, setAccount] = useState({ firstName: "", lastName: "", email: "", username: "", password: "" });
+  var [errors, setErrors] = useState({
+    firstName: "required",
+    lastName: "",
+    email: "",
+    username: "must be between 4 to 8 characters",
+    password: "required",
+  });
+  var [fresh, setFresh] = useState({
+    firstName: true,
+    lastName: true,
+    email: true,
+    username: true,
+    password: true,
+  });
 
   var submit = (e) => {
     // props.history.push("about");
@@ -40,25 +54,40 @@ function Register(props) {
         </div>
         <form onSubmit={submit}>
           <div className="names">
-            <div className="field firstName">
-              <label htmlFor="firstName">First Name</label>
+            <div className={`field firstName ${!fresh.firstName && errors.firstName ? "error" : ""}`}>
+              <label htmlFor="firstName">
+                First Name
+                {!fresh.firstName && errors.firstName ? <span>{errors.firstName}</span> : ""}
+              </label>
               <input name="firstName" onChange={handleChange} id="firstName"></input>
             </div>
-            <div className="field lastName">
-              <label htmlFor="lastName">Last Name</label>
+            <div className={`field lastName ${!fresh.lastName && errors.lastName ? "error" : ""}`}>
+              <label htmlFor="lastName">
+                Last Name
+                {!fresh.lastName && errors.firstName ? <span>{errors.lastName}</span> : ""}
+              </label>
               <input name="lastName" onChange={handleChange} id="lastName"></input>
             </div>
           </div>
-          <div className="field email">
-            <label htmlFor="email">Email</label>
+          <div className={`field email ${!fresh.email && errors.email ? "error" : ""}`}>
+            <label htmlFor="email">
+              Email
+              {!fresh.email && errors.email ? <span>{errors.email}</span> : ""}
+            </label>
             <input name="email" onChange={handleChange} id="email"></input>
           </div>
-          <div className="field username">
-            <label htmlFor="username">Username</label>
+          <div className={`field username ${!fresh.username && errors.username ? "error" : ""}`}>
+            <label htmlFor="username">
+              Username
+              {!fresh.username && errors.username ? <span>{errors.username}</span> : ""}
+            </label>
             <input name="username" onChange={handleChange} value={account.username} id="username"></input>
           </div>
-          <div className="field password">
-            <label htmlFor="password">Password</label>
+          <div className={`field password ${!fresh.password && errors.password ? "error" : ""}`}>
+            <label htmlFor="password">
+              Password
+              {!fresh.password && errors.password ? <span>{errors.password}</span> : ""}
+            </label>
             <input name="password" onChange={handleChange} value={account.password} id="password"></input>
           </div>
           <button>Sign Up</button>
