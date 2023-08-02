100ba68482048c02ddb531649ba3f3b16a4ba43d
Mattthew Lusty <mattlusty123@gmail.com>
2023-08-02 16:46:34 +0100

    changed contact

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -5,8 +5,8 @@ function ContactPage(props) {
   return (
     <div className="ContactPage">
       <div className="ContactPage_header">Contact</div>
-      <div className="ContactPage_content">Email: martinloosty189@gmail.com</div>
-      <div className="ContactPage_content">Mobile: 07763 638993</div>
+      <div className="ContactPage_content">Email: mattlusty123@gmail.com</div>
+      <div className="ContactPage_content">Mobile: 07821673724</div>
       <div className="ContactPage_content">Send me a message: </div>
     </div>
   );
