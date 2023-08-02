a366f24ea239992a1072a9603958dcd0bfe23ea7
Mattthew Lusty <mattlusty123@gmail.com>
2023-08-02 15:18:38 +0100

    yes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ function Contact(props) {
   return (
     <div className="Contact">
       <div className="header">Contact</div>
-      <div className="content">Email: martinloosty189@gmail.com</div>
-      <div className="content">Mobile: 07763 638993</div>
+      <div className="content">Email: mattlusty123@gmail.com</div>
+      <div className="content">Mobile: 07821673724</div>
       <div className="content">Send me a message: </div>
     </div>
   );
